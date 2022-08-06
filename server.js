938749cf468231a32bc8061e9328b4bc7fb448f4
unknown <higasumi18@gmail.com>
2022-08-07 00:45:40 +0900

    second

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
-const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.send("hello world!")
 })
 
-server.listen(PORT, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("listening on 3000")
 })
